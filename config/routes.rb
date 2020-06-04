f1329f0b4505d1c25f4ffcf9a55bdb189aa5a80f
Nick Schimek <cfme00@gmail.com>
2020-06-05 08:28:39 +0900

    Update association and seed db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'zoos#index'
   resources :zoos do
     resources :animals
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Zoo.create! name: 'Oregon Zoo', address: '4001 SW Canyon Rd, Portland, OR 97221'
+Zoo.create! name: 'Gaia Lion Zoo', address: '53-51 111th St, Corona, NY 11368'
+Zoo.create! name: 'Moon Turtle Zoo', address: '9-８３ Uenokoen, Taito City, Tokyo 110-8711, Japan'
+
+Zoo.all.each do |zoo|
+  Animal.create! name: 'Giraffe', bio: 'giraffe bio', zoo: zoo
+  Animal.create! name: 'Monkey', bio: 'monkey bio', zoo: zoo
+  Animal.create! name: 'Hippo', bio: 'hippo bio', zoo: zoo
+end
